9088398e67ba23969561d2a83e3045b3153393c9
Marielle Rectin <157998090+onlymarkive@users.noreply.github.com>
2024-12-20 22:14:55 +0800

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -20,17 +20,27 @@ CREATE TABLE cart (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+INSERT INTO users (username, password) VALUES 
+('mawrkive', 'password123'),
+('mawee', 'password456'),
+('rkive', 'password789');
+
+INSERT INTO cart (user_id, item_name, item_type, quantity, price, total_cost) VALUES
+(1, 'Milk', 'Grocery', 4, 75.00, 300.00),
+(1, 'T-shirt', 'Clothing', 1, 350.00, 350.00),
+(2, 'Notebook', 'Stationery', 3, 50.00, 150.00),
+(3, 'Charger', 'Electronics', 1, 350.00, 350.00);
+
 SHOW TABLES;
 
 SELECT * FROM users;
+
 SELECT * FROM cart WHERE user_id = 1;
 
 SHOW DATABASES;
-
 
 SELECT * FROM users;
 
 SELECT * FROM cart;
 
 SELECT * FROM cart WHERE user_id = 1;
-
